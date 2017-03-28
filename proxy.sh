319ccf65694e4c298c4aa1e3ed92a24e9b19e1bc
floatsd <float.hellowmr@gmail.com>
2017-03-29 00:34:24 +0800

    add proxy and folders

---
+++ proxy.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Script to randomly set Background from files in a directory
+
+# Directory Containing Pictures
+DIR="/home/username/Pictures/background"
+
+# Command to Select a random jpg file from directory
+# Delete the *.jpg to select any file but it may return a folder
+PIC=$(ls $DIR/*.jpg | shuf -n1)
+
+# Command to set Background Image
+gconftool -t string -s /desktop/gnome/background/picture_filename $PIC
